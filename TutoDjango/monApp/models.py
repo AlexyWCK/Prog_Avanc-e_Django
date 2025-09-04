0a1a90d625600d6bfd42ab51bbc799317fb34c11
Wiciak Alexy <o22300674@or-iut-i003-f30.iut45.univ-orleans.fr>
2025-09-04 10:23:15 +0200

    finalisation petit exercice

--- TutoDjango/monApp/models.py
+++ TutoDjango/monApp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+class Categorie(models.Model):
+    idCat = models.AutoField(primary_key=True)
+    nomCat = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nomCat
+    
+class Statut(models.Model):
+    idStatut = models.AutoField(primary_key=True)
+    libelle = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.libelle
+
+
+class Produit(models.Model):
+    refProd = models.AutoField(primary_key=True)
+    intituleProd = models.CharField(max_length=200)
+    prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits", null=True, blank=True)
+    rayons = models.ManyToManyField("Rayon", related_name="produits")  # relation Many-to-Many
+    dateFabrication = models.DateField(null=True, blank=True)
+    statut = models.ForeignKey(Statut, on_delete=models.SET_NULL, null=True, blank=True, related_name="produits")
+
+    
+    # Relation : un produit appartient à une catégorie
+    categorie = models.ForeignKey(
+        Categorie,
+        on_delete=models.CASCADE,
+        related_name="produits",
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.intituleProd
+    
+    
+class Rayon(models.Model):
+    idRayon = models.AutoField(primary_key=True)
+    nomRayon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nomRayon
