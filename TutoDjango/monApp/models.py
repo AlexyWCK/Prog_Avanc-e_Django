7bc2295dbc0708b551da01a8f9269ad29c24c8b6
AlexyWCK <you@example.com>
2025-09-16 17:58:35 +0200

    ajout de la Table contenir (Many to Many)

--- TutoDjango/monApp/models.py
+++ TutoDjango/monApp/models.py
@@ -7,6 +7,7 @@ class Categorie(models.Model):
     def __str__(self):
         return self.nomCat
     
+
 class Statut(models.Model):
     idStatut = models.AutoField(primary_key=True)
     libelle = models.CharField(max_length=100)
@@ -14,35 +15,36 @@ class Statut(models.Model):
     def __str__(self):
         return self.libelle
     
-#Essaie de la relation Many-to-Many
-
 
 class Produit(models.Model):
     refProd = models.AutoField(primary_key=True)
     intituleProd = models.CharField(max_length=200)
     prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits", null=True, blank=True)
-    rayons = models.ManyToManyField("Rayon", related_name="produits")  # relation Many-to-Many
     dateFabrication = models.DateField(null=True, blank=True)
     statut = models.ForeignKey(Statut, on_delete=models.SET_NULL, null=True, blank=True, related_name="produits")
-
-    
-    # Relation : un produit appartient à une catégorie
-    categorie = models.ForeignKey(
-        Categorie,
-        on_delete=models.CASCADE,
-        related_name="produits",
-        null=True,
-        blank=True
-    )
 
     def __str__(self):
         return self.intituleProd
     
-    
+
 class Rayon(models.Model):
     idRayon = models.AutoField(primary_key=True)
     nomRayon = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nomRayon
+    
+
+#Ajout de la table Contenir
+class Contenir(models.Model):
+    pk = models.CompositePrimaryKey("produit_id", "rayon_id")
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    rayon = models.ForeignKey(Rayon, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Contenir"
+        verbose_name_plural = "Contenir"
+
+    def __str__(self):
+        return f"{self.produit.intituleProd} dans {self.rayon.nomRayon}"
