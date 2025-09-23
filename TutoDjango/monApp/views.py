186d32b4c547a8c0625dbffb876cb86507a3541e
AlexyWCK <you@example.com>
2025-09-23 18:07:49 +0200

    script lancement + login/logout fonctionnel

--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import Produit, Categorie, Statut
+from django.urls import reverse
 
 class HomeView(TemplateView):
     template_name = "monApp/page_home.html"
@@ -69,19 +70,15 @@ class ConnectView(LoginView):
     template_name = 'monApp/page_login.html'
 
     def post(self, request, **kwargs):
-        lgn = request.POST.get('username', False)
-        pswrd = request.POST.get('password', False)
+        lgn = request.POST.get('username')
+        pswrd = request.POST.get('password')
         user = authenticate(username=lgn, password=pswrd)
         if user is not None and user.is_active:
             login(request, user)
-            return render(request, 'monApp/page_home.html', {
-                'param': lgn,
-                'titreh1': "Hello DJANGO",
-                'titretitre': "Accueil",
-                'page_home': True
-            })
+            return redirect(reverse('monApp:home'))
         else:
             return render(request, 'monApp/page_register.html')
+
 
 class RegisterView(TemplateView):
     template_name = 'monApp/page_register.html'
@@ -98,3 +95,4 @@ class RegisterView(TemplateView):
 
 class DisconnectView(LogoutView):
     next_page = 'monApp:home'
+
